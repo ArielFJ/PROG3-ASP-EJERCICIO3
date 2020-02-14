f3a3400fbf0e020183cab72fd20d5223040864db
Ariel <arielfj@magnanym.com>
2020-02-14 14:29:31 -0400

    Modelos Carros y DealerViewModel añadidos

---
+++ Ejercicio3/Models/Carro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio3.Models
+{
+    public class Carro
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        public ConsoleColor Color { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Marca { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Chasis { get; set; }
+    }
+}
---
+++ Ejercicio3/Models/DealerViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio3.Models
+{
+    public class DealerViewModel
+    {
+
+        private static List<Carro> carros;
+        public List<Carro> Carros
+        {
+            get
+            {
+                if (carros == null)
+                {
+                    carros = new List<Carro>();
+                }
+                return carros;
+            }
+        }
+
+    }
+}
